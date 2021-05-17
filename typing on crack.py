c928fc38dfd1ae4ad727326fa69dc262d772a215
Lucas do Drip <77930083+lucascompython@users.noreply.github.com>
2021-05-17 01:40:47 +0100

    Add files via upload

---
+++ typing on crack.py
@@ -0,0 +1,48 @@
+from pynput.keyboard import Controller, Key, Listener
+from pynput import mouse
+import time
+import pyscreenshot
+import pytesseract
+import keyboard
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#getting the area for the screenshot
+keyboard.wait("esc")
+def on_click(x, y, button, pressed):
+    if pressed:
+        on_click.px = x
+        on_click.py = y
+    else:
+        on_click.rx = x
+        on_click.ry = y
+    
+    if not pressed:
+        return False
+
+
+with mouse.Listener(on_click=on_click) as listener:
+    listener.join()
+
+img = pyscreenshot.grab(bbox = (on_click.px, on_click.py, on_click.rx, on_click.ry))
+img.save("img.png")
+
+txt = str(pytesseract.image_to_string(img, lang = "eng")).replace("|", "I")
+txt.replace("‘", "'")
+txt = " ".join(txt.splitlines())
+print(txt)
+
+keyboard = Controller()
+
+
+#typing when numlock is pressed
+def on_press(key): 
+
+    if key == Key.num_lock:
+        for i in txt:
+            keyboard.press(i)
+            keyboard.release(i)
+            time.sleep(0.05)
+    
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
